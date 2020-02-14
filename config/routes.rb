ebedf049d78c4f06fbfce9dceddeb20b3b1fb439
Cillian Wing <cwing@zagmail.gonzaga.edu>
2020-02-13 21:37:13 -0600

    Created BooksController and BookshelvesController with basic CRUD methods (yet to be filled in). Updated routes to include these 2 controller actions.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do
   	namespace :v1 do
   		resource :users, only: [:create]
+  		resource :books
+  		resource :bookshelves
   		post "/login", to: "auth#login"
   		get "/auto_login", to: "auth#auto_login"
   		get "/user_is_authed", to: "auth#user_is_authed"
